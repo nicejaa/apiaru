220f1bea0cdf374df0be9355773f69e37605789c
nicejaa <nice8844@gmail.com>
2024-03-21 22:38:11 +0700

    fix cors

--- routes/book.router.js
+++ routes/book.router.js
@@ -1,9 +1,10 @@
+import cors from "cors";
 const express = require("express");
 const router = express.Router();
 
 const bookController = require("../controllers/book.controller");
 
-router.get("/getAll", bookController.getAll);
+router.get("/getAll", cors(), bookController.getAll);
 router.get("/detail/:id", bookController.getById);
 router.post("/add", bookController.create);
 router.put("/update", bookController.updateById);
